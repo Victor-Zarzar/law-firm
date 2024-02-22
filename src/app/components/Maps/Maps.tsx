a83d650928d5b271ddf39e2b88f46bd9c76745be
victorzarzar <victorzarzar58@gmail.com>
2024-02-22 15:52:23 -0300

    implement lib maps pigeon

---
+++ src/app/components/Maps/Maps.tsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import { Map, Marker } from "pigeon-maps";
+
+const position: [number, number] = [-30.034647, -51.217658];
+
+export default function MapsInfo() {
+    return (
+        <div className="h-44 w-[20rem] sm:h-52 sm:w-[25rem] md:h-96 md:w-[70rem]">
+            <Map defaultCenter={position} defaultZoom={10}>
+                <Marker width={50} anchor={position} />
+            </Map>
+        </div>
+    );
+}
--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,7 +1,14 @@
+'use client'
 import React from 'react'
+import MapsInfo from '../components/Maps/Maps'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='h-screen'>
+      <h1 className="font-bold text-3xl md:text-4xl leading-10 mb-10 tracking-tight text-black text-center cursor-default mt-20">Nossa Localização:</h1>
+      <div className="flex justify-center items-center mt-0 mb-20">
+        <MapsInfo />
+      </div>
+    </div>
   )
 }
