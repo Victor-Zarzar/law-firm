cb55dabb58d26661bb40b8eee6a1122ec8e2fbc0
victorzarzar <victorzarzar58@gmail.com>
2024-04-05 14:58:36 -0300

    feat: add new button click on whatsapp, and refactor: responsive header

---
+++ src/app/components/ButtonWhatsapp/ButtonWhatsapp.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import React from 'react';
+
+export default function ButtonWhatsapp() {
+    return (
+        <button
+            aria-label="Botão para chamada no whatsapp"
+            className="flex px-6 py-3 gap-3
+ items-center mx-auto justify-center text-xs md:text-base w-60 md:w-1/3 bg-green-600 hover:bg-green-700 text-slate-50 rounded-md">
+            <a
+                href="https://api.whatsapp.com/send?phone=5551996180483"
+                target="_blank"
+                rel="noreferrer">
+                Quero ajuda especializada
+            </a>
+            <Image
+                 src="/iconWhatsapp.svg"
+                alt="icone whatsapp"
+                width={20}
+                height={20}
+                className="text-white "
+            />
+        </button>
+    )
+}
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,32 +2,34 @@ import Image from 'next/image'
 import React from 'react'
 import CardHome from '../Card/CardHome'
 import Expertise from '../Expertise/Expertise'
+import ButtonWhatsapp from '../ButtonWhatsapp/ButtonWhatsapp'
 
 export default function Header() {
 
   return (
     <div className='relative justify-center'>
       <div className='flex justify-center items-center'>
-      <Image
-            src="/banner.png"
-            alt="logo"
-            width={800}
-            height={400}
-            priority
-            className="h-44 w-full md:w-10 md:h-10 lg:w-full lg:h-[35rem]"
-          />
-        <div className="absolute left-2 md:left-40">
+        <Image
+          src="/banner.png"
+          alt="logo"
+          width={800}
+          height={400}
+          priority
+          className="h-44 w-full md:h-[33rem] md:w-full"
+        />
+        <div className="absolute left-2 md:left-20 lg:left-32">
           <Image
             src="/logo.png"
             alt="logo"
             width={400}
             height={800}
             priority
-            className="h-40 w-40 md:w-10 md:h-10 lg:w-[35rem] lg:h-[35rem]"
+            className="h-40 w-40 md:w-72 md:h-72 lg:w-3/4 lg:h-5/6"
           />
         </div>
       </div>
       <CardHome />
+      <ButtonWhatsapp />
       <Expertise />
     </div>
   )
